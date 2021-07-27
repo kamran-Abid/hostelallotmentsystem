266db3d46fc94092048f0404ad43cd35189a9a6f
Kamran Abid <52425058+kamran-Abid@users.noreply.github.com>
2021-07-27 19:29:46 +0500

    Delete client directory

--- client/src/reducer/empReducer.js
+++
@@ -1,7 +0,0 @@
-export const initialState_emp = null;
-export const reducer_emp = (state_emp, action) => {
-    if (action.type === "EMP") {
-        return action.payload;
-    }
-    return state_emp;
-}
